3021cb262d559f32b62a40a3eb8de09cc3ac998a
Vianka Rivera <viankalrivera@gmail.com>
2020-05-21 14:20:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,12 @@
 class Triangle
   # write code here
-  def initialize(equilateral, isosceles, scalene)
-    @equilateral = equilateral
-    @isosceles = isosceles
-    @scalene = scalene
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
   end
 
   def kind
+    if @
   end
 end
